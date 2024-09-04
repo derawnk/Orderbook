f7210f34d6ce545137a224b26de23e5353edad7c
derawnk <derawnk@gmail.com>
2024-09-04 01:15:16 -0400

    Add basic structure for order book system
    - Define enums for OrderType and Side
    - Create type aliases for Price, Quantity, and OrderId
    - Implement LevelInfo struct to represent price and quantity
    - Add OrderbookLevelInfos class to manage bids and asks
    - Set up empty main function

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -23,3 +23,50 @@
 #include <tuple>
 #include <format>
 
+enum class OrderType 
+{
+    GoodTllCancelled,
+    FillAndKill
+};
+
+enum class Side 
+{
+    Buy,
+    Sell
+};
+
+using Price = std::int32_t;
+using Quantity = std::uint32_t;
+using OrderId = std::uint64_t;
+
+struct LevelInfo
+{
+    Price price_;
+    Quantity quantity_;
+};
+
+using LevelInfos = std::vector<LevelInfo>;
+
+class OrderbookLevelInfos 
+{
+    public:
+    OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks)
+    : bids_{bids}
+    , asks_{asks}
+    {}
+
+    const LevelInfos& GetBids() const {return bids_;}
+    const LevelInfos& GetAsks() const {return asks_;}
+
+    private:
+    LevelInfos bids_;
+    LevelInfos asks_;
+
+};
+
+
+int main()
+{
+
+return 0;
+};
