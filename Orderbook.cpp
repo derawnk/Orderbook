150edc7ed41187e60c62f16839e3839a0720bef3
derawnk <derawnk@gmail.com>
2024-09-14 21:49:24 -0400

    Implement core classes for order book system
    - Add Order class with essential order attributes and methods
    - Create OrderModify class for handling order modifications
    - Implement Trade and TradeInfo structures for executed trades
    - Introduce smart pointers (shared_ptr) for Order management
    - Use STL containers: list for OrderPointers, vector for Trades
    - Add using namespace std directive

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -23,6 +23,9 @@
 #include <tuple>
 #include <format>
 
+
+using namespace std;
+
 enum class OrderType 
 {
     GoodTllCancelled,
@@ -64,9 +67,107 @@ class OrderbookLevelInfos
 
 };
 
+class Order 
+{
+    public:
+    Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+    : orderType_{orderType}
+    , orderId_{orderId}
+    , side_{side}
+    , price_{price}
+    , initialQuantity_{quantity}
+    , remainingQuantity_{quantity}
+    {}
+
+    OrderId GetOrderId() const {return orderId_;}
+    Side GetSide() const {return side_;}
+    Price GetPrice() const {return price_;}
+    OrderType GetOrderType() const {return orderType_;}
+    Quantity GetInitialQuantity() const {return initialQuantity_;}
+    Quantity GetRemainingQuantity() const {return remainingQuantity_;}
+    Quantity GetFilledQuantity() const {return GetInitialQuantity() - GetRemainingQuantity();}
+    void Fill(Quantity quantity) 
+    {
+
+    }
+
+private:
+OrderType orderType_;
+OrderId orderId_;
+Side side_;
+Price price_;
+Quantity initialQuantity_;
+Quantity remainingQuantity_;
+};
+
+using OrderPointer = shared_ptr<Order>;
+using OrderPointers = list<OrderPointer>;
+
+class OrderModify 
+{
+    public:
+    OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
+    : orderId_{orderId}
+    , side_{side}
+    , price_{price}
+    , quantity_{quantity}
+    {}
+
+    OrderId GetOrderId() const {return orderId_;}
+    Side GetSide() const {return side_;}
+    Price GetPrice() const {return price_;}
+    Quantity GetQuantity() const {return quantity_;}
+
+    OrderPointer ToOrderPointer(OrderType type) const
+    {
+        return make_shared<Order>(type, GetOrderId(), GetSide(), GetPrice(), GetQuantity());
+    }
+
+    private:
+    OrderId orderId_;
+    Side side_;
+    Price price_;
+    Quantity quantity_;
+};
+
+struct TradeInfo 
+{
+    OrderId orderId_;
+    Price price_;
+    Quantity quantity_;
+};
+
+class Trade 
+{
+    public:
+    Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+    : bidTrade_{bidTrade}
+    , askTrade_{askTrade}
+    {}
+
+    const TradeInfo& GetBidTrade() const {return bidTrade_;}
+    const TradeInfo& GetAskTrade() const {return askTrade_;}
+
+    private:
+    TradeInfo bidTrade_;
+    TradeInfo askTrade_;
+};
+
+using Trades = vector<Trade>;
 
 int main()
 {
 
 return 0;
 };
+
+
+
+
+
+/* Code for error message. Needs to be reworked.
+if (quantity > GetRemainingQuantity())
+        throw logic_error(format("Order ({}) cannot be filled for than its remaining quantity.", GetOrderId() ));
+
+        remainingQuantity_ -= quantity;
+*/
